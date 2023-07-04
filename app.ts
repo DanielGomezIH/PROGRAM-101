f62f7758c1c958bc2f7e7a83d6bcf07dfb46fefa
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-04 14:54:32 -0500

    Ejemplos de tipos de datos 1

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("Hola Mundo");
+// Strings
+
+let firstName: string = "Daniel";
+let lastName: string = "Gómez";
+
+// console.log(firstName, lastName);
+
+let fullName: string = firstName + " " + lastName;
+
+console.log(fullName);
+
+let number1: number = 10;
+let number2: number = 20;
+
+console.log(number1 + number2);
