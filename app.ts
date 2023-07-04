a7f6db93585e8c8ba5ddf98b7df1dd504ad8e5e1
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-04 16:28:44 -0500

    Ejemplos de tipos de datos 2 Booleanos

--- app.ts
+++ app.ts
@@ -9,7 +9,21 @@ let fullName: string = firstName + " " + lastName;
 
 console.log(fullName);
 
-let number1: number = 10;
-let number2: number = 20;
+// Numbers
+
+let number1: number = 10.5;
+let number2: number = 20.5;
 
 console.log(number1 + number2);
+
+// Booleanos
+
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+
+isActive = !!isActive;
+
+console.log("isActive", isActive);
+console.log("isRunning", isRunning);
+console.log("hasKids", hasKids);
