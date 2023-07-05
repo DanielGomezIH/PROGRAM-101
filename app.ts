9e2d9d001718257dd0585d12b6135ce41a6befa7
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-05 13:50:09 -0500

    Ejemplo Estructura de control IF

---
+++ app.ts
@@ -0,0 +1,15 @@
+console.log("Inicio de programa");
+
+let isTired: boolean = true;
+
+if (isTired) {
+  console.log("Tomar café");
+} else {
+  console.log("Tomar agua");
+}
+
+// if (!isTired) {
+// console.log("Tomar agua");
+// }
+
+console.log("Fin de programa");
