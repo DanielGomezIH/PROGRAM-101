7bb4672693d0ca41c3ea20da7a35953da7140f25
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-05 14:27:03 -0500

    If Else anidado y Else If

--- app.ts
+++ app.ts
@@ -1,15 +1,29 @@
 console.log("Inicio de programa");
 
-let isTired: boolean = true;
+// IF ELSE
 
-if (isTired) {
-  console.log("Tomar café");
-} else {
-  console.log("Tomar agua");
-}
+// let isTired: boolean = true;
+
+// if (isTired) {
+//   console.log("Tomar café");
+// } else {
+//   console.log("Tomar agua");
+// }
 
 // if (!isTired) {
 // console.log("Tomar agua");
-// }
+// }]
+
+// IF ELSE ANIDADO y ELSE IF
+
+let grade: number = 50;
+
+if (grade >= 60) {
+  console.log("El alumno aprueba la clase");
+} else if (grade >= 50) {
+  console.log("Por favor estudie más");
+} else {
+  console.log("El alumno NO aprueba la clase");
+}
 
 console.log("Fin de programa");
