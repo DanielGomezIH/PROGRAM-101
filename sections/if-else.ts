21f716506f7433371f4c82455038ab10b482d3ec
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-05 14:29:21 -0500

    If-else.ts creado y app.ts limpio

---
+++ sections/if-else.ts
@@ -0,0 +1,29 @@
+console.log("Inicio de programa");
+
+// IF ELSE
+
+// let isTired: boolean = true;
+
+// if (isTired) {
+//   console.log("Tomar café");
+// } else {
+//   console.log("Tomar agua");
+// }
+
+// if (!isTired) {
+// console.log("Tomar agua");
+// }]
+
+// IF ELSE ANIDADO y ELSE IF
+
+export let grade: number = 50;
+
+if (grade >= 60) {
+  console.log("El alumno aprueba la clase");
+} else if (grade >= 50) {
+  console.log("Por favor estudie más");
+} else {
+  console.log("El alumno NO aprueba la clase");
+}
+
+console.log("Fin de programa");
