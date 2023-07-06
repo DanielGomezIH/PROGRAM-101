8b09e4449626aeb54af7ef5c911c042f38d032c4
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-06 12:44:51 -0500

    Ciclo Do While

--- app.ts
+++ app.ts
@@ -1,11 +1,23 @@
 // Ciclo While
 
-let gasTank = 20;
+// let gasTank = 20;
 
-while (gasTank > 0) {
+// while (gasTank > 0) {
+//   console.log("Gasolina restante", gasTank);
+
+//   gasTank--;
+// }
+
+// console.log("Ya no tiene gasolina");
+
+// Ciclo Do While
+
+let gasTank = 50;
+
+do {
   console.log("Gasolina restante", gasTank);
 
   gasTank--;
-}
+} while (gasTank > 0);
 
 console.log("Ya no tiene gasolina");
