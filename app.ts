87136bd19ff4b346eb9707a98f446b5f1a074f68
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-06 13:43:21 -0500

    Ciclo For

---
+++ app.ts
@@ -0,0 +1,17 @@
+//Ciclo For
+
+let limite = 10;
+
+for (let i: number = 1; i <= limite; i++) {
+  if (i === 5) {
+    continue;
+  }
+
+  console.log("let i =", i);
+
+  if (i === 5) {
+    break;
+  }
+}
+
+console.log("Fin del programa");
