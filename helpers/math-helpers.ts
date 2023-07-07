1a8624c341a0788528d108b3010b7d5acf203574
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-07 14:23:17 -0500

    math-helpers.ts creado

---
+++ helpers/math-helpers.ts
@@ -0,0 +1,22 @@
+// Add
+export function addTwoNumbers(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+// Substract
+
+export function substractTwoNumbers(num1: number, num2: number): number {
+  return num1 - num2;
+}
+
+//Times
+
+export function timesTwoNumbers(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+// Divide
+
+export function divideTwoNumbers(num1: number, num2: number): number {
+  return num1 / num2;
+}
