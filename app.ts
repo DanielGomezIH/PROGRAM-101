ab942b3898f1cf67a843ef552a328041805ced11
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-10 14:30:39 -0500

    arrays.ts creado y app.ts limpio

--- app.ts
+++
@@ -1,5 +0,0 @@
-let flowers: string[] = ["Rosa", "Girasol", "Lirio"];
-
-for (let i = 0; i < flowers.length; i++) {
-  console.log(flowers[i]);
-}
---
+++ sections/arrays.ts
@@ -0,0 +1,12 @@
+export let employees: string[] = ["Fernando", "Melissa", "Eduardo"];
+let salaries: number[] = [1500, 2400, 3200];
+
+for (let i = 0; i < employees.length; i++) {
+  console.log(employees[i], "tiene un salario de", salaries[i]);
+}
+
+// let flowers: string[] = ["Rosa", "Girasol", "Lirio"];
+
+// for (let i = 0; i < flowers.length; i++) {
+//   console.log(flowers[i]);
+// }
