c330aeca2181301133d7f3f698eb2cda52604151
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-12 14:14:49 -0500

    homework-3.ts creado

---
+++ homeworks/homework-3.ts
@@ -0,0 +1,7 @@
+// 3- Calcule el área de un triangulo rectángulo que:
+// Base 10 y altura 5
+
+export let base = 12;
+export let altura = 5;
+
+console.log("El área del triángulo rectángulo es:", (base * altura) / 2);
