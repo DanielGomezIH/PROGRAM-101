31d2776c1c2e4d503ee06157589b5102e79164c4
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-12 11:08:14 -0500

    pounds-kgs..ts modificado

--- helpers/pounds-kgs.ts
+++ helpers/pounds-kgs.ts
@@ -1,5 +1,5 @@
 export function convertPoundsKgs(pounds: number) {
   let libras = pounds;
   let kgs = 0.453592;
-  console.log(pounds, "libras es igual a", kgs * pounds, "kilogramos");
+  console.log(pounds, "libras es igual a:", kgs * pounds, "kilogramos");
 }
