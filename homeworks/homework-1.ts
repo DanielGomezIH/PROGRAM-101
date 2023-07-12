5ab2b94bf3ee83efc9b79c5ee52b15bd63dca5ca
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-12 11:09:56 -0500

    homework-1.ts creado y app.ts limpio

---
+++ homeworks/homework-1.ts
@@ -0,0 +1,12 @@
+import { convertPoundsKgs } from "../helpers/pounds-kgs";
+
+// 1- Convertir de Libras a Kilogramos. (1 libra = 0.453592 Kilogramos)
+// Ejemplo: 150 libras = 68.0388 kilogramos.
+
+let libras = 150;
+
+let kgs = 0.453592 * 150;
+
+console.log(libras, "libras es igual a:", kgs, "Kilogramos");
+
+// convertPoundsKgs(150);
