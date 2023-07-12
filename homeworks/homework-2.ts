a7640201de729c5dff4e9b7d556dba65a8a8e42a
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-12 14:06:48 -0500

    homework-2.ts creado

---
+++ homeworks/homework-2.ts
@@ -0,0 +1,8 @@
+// 2- Convertir de kilómetros a millas, sabiendo que: (1 kilómetro es igual a 0.62 millas )
+// Ejemplo: 10 kilómetros = 6.21 millas
+
+export let kmts = 10;
+
+export let miles = kmts * 0.62;
+
+console.log(kmts, "kilometros es igual a:", miles, "millas");
